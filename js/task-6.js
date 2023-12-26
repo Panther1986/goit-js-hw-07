d6d0c3415c2dfe2bd37b18112821dceecc976b1f
Panther1986 <polzovatel77@gmail.com>
2023-12-26 15:58:43 +0200

    update

--- js/task-6.js
+++ js/task-6.js
@@ -1,3 +1,43 @@
+const elementDiv = document.querySelector('#controls');
+const dataCreate = document.querySelector('.btn-create');
+const elementBoxes = document.querySelector('#boxes');
+const dataDestroy = document.querySelector('.btn-destroy');
+
+
+const inputEl = document.querySelector('input');
+const createNewEl = document.createElement("div");
+
+dataCreate.addEventListener('click', createInput);
+
+function createInput() {
+  const amount = inputEl.value;
+  if (amount > 1 && amount <= 100) {
+    createBoxes(amount);
+  }
+
+}
+
+
+
+function createBoxes(amount) {
+  for (let i = 0; i < amount; i++) {
+    const newDiv = createNewEl.cloneNode();
+    
+      elementBoxes.appendChild(newDiv);
+  }
+
+}
+
+console.log(newDiv);
+console.log(elementBoxes);
+
+dataDestroy.addEventListener('click', destroyBoxes);
+
+function destroyBoxes() {
+  elementBoxes.innerHTML = '';
+  elementBoxes.remove();
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
